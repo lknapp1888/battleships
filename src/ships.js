0ab3f248d396fe1105f48812f9764c24b367fcbd
Lewis Knapp <lknapp1888@gmail.com>
2023-03-12 13:22:16 +0000

    add ship factory and tests

--- src/ships.js
+++ src/ships.js
@@ -1,3 +1,19 @@
-export const Ship = (length) => {
-    return {length}
+export class Ship {
+  constructor(length) {
+    this.length = length;
+    this.hitCount = 0;
+  }
+
+  getLength() {
+    return this.length;
+  }
+  hit() {
+    this.hitCount++
+  }
+  isSunk() {
+    if (this.hitCount >= this.length) {
+        return true;
+    }
+    return false;
+  }
 }
--- src/tests/ships.test.js
+++ src/tests/ships.test.js
@@ -1,5 +1,33 @@
 import { Ship } from "../ships"
 
-test('test ship function', ()=> {
-    expect(Ship(4).length).toBe(4)
+describe('Ship class', ()=> {
+   let fourLength;
+    beforeEach(() => {
+        fourLength = new Ship(4)
+    })
+   test("defines getLength()", () => {
+    expect(typeof fourLength.getLength).toBe("function");
+  });
+    test('length should reflect initialised length', () => {
+        expect(fourLength.getLength()).toBe(4)
+    })
+    test('defines hit()', () => {
+        expect(typeof fourLength.hit).toBe("function");
+    })
+    test('hit() should register hits on the hitcount property', () => {
+        fourLength.hit()
+        fourLength.hit()
+        expect(fourLength.hitCount).toBe(2)
+    })
+    test('defines isSunk()', () => {
+        expect(typeof fourLength.isSunk).toBe("function");
+    })
+    test('expect hits equal or greater than length to sink ship', () => {
+        fourLength.hit()
+        fourLength.hit()
+        expect(fourLength.isSunk()).toBeFalsy()
+        fourLength.hit()
+        fourLength.hit()
+        expect(fourLength.isSunk()).toBeTruthy()
+    })
 })
